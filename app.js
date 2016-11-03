5805da2d2d22a349e9382307e08790bc564adfaf
sjpfeiffer <stevenjamespfeiffer@gmail.com>
2016-11-03 06:27:01 +0430

    Implemented user feedback and cleaned up the code. Added comments as well.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('.js-gears').hide()
+    $('.js-gears').hide();
 
     //STATE OBJECT
     var state = {
@@ -19,52 +19,49 @@ $(document).ready(function() {
     $('#js-search-form').submit(function(e) {
         e.preventDefault();
         e.stopPropagation();
-        $('.no-results').html('')
-        $('.js-gears').removeClass('hidden').fadeIn(100)
-        $('.col-4').hide()
+        $('.no-results').html('');
+        $('.js-gears').removeClass('hidden').fadeIn(100);
+        $('.col-4').hide();
         resetState();
         getArtistId($('#search-input').val());
     });
 
     //FUNCTIONS THAT MODIFY STATE
     var resetState = function() {
-            state.artistId = [];
-            state.similarArtistId = [];
-            state.similarArtistNames = [];
-            state.similarArtistImages = [];
-            state.allImageUrls = [];
-            state.topTrackNames = [];
-            state.topTracksYouTube = [];
-            state.previewUrl = [];
-            // state.numberOfResults = 0;
-            state.resubmit = 0;
-        }
-        //SPOTIFY API
+        state.artistId = [];
+        state.similarArtistId = [];
+        state.similarArtistNames = [];
+        state.similarArtistImages = [];
+        state.allImageUrls = [];
+        state.topTrackNames = [];
+        state.topTracksYouTube = [];
+        state.previewUrl = [];
+        state.resubmit = 0;
+    };
+    //SPOTIFY API
+    //Gets the artist ID of what the user input into the search, then stores it in the state.
     var getArtistId = function(q) {
         return $.get('https://api.spotify.com/v1/search', {
                 q: q,
                 type: "artist"
             })
             .then(function(result) {
-                // console.log(result.artists.items.length)
-                if (result.artists.items.length == 0) {
-                    // console.log('if');
+                if (result.artists.items.length === 0) {
                     noResults('.no-results');
                 } else if (result.artists.items.length >= 1) {
-                    // console.log('else if')
                     state.artistId.push(result.artists.items[0].id);
                     getSimilarArtists(state.artistId[0]);
 
-                };
+                }
             });
     };
+    //Takes the artist ID from the previous function and searches for similar artists.
+    //Then stores the similar artist IDs in the state.
     var getSimilarArtists = function(id) {
-        // console.log('similarartists')
         return $.get('https://api.spotify.com/v1/artists/' + id + '/related-artists', {
             type: "artist",
             limit: '16'
         }).then(function(result) {
-            // state.numberOfResults = result.artists.length
             result.artists.forEach(function(artists) {
                 state.similarArtistId.push(artists.id);
                 if (state.similarArtistId.length == state.numberOfResults) {
@@ -73,8 +70,7 @@ $(document).ready(function() {
             });
         });
     };
-
-
+    //Takes the similar artist IDs and stores the artists name, the url for the album cover, and the name of the top track in the state.
     var getArtworkAndName = function(id) {
         id.forEach(function(result) {
             return $.get('https://api.spotify.com/v1/artists/' + result + '/top-tracks', {
@@ -82,29 +78,26 @@ $(document).ready(function() {
                     popularity: '100'
                 })
                 .done(function(result) {
-                    // console.log ("GETTING ARTWORK AND NAME")
-                    if (result.tracks[0] != null) {
-                        state.similarArtistNames.push(result.tracks[0].artists[0].name)
-                        console.log (state.similarArtistNames.length)
-                        state.similarArtistImages.push(result.tracks[0].album.images[0].url)
-                        state.topTrackNames.push((result.tracks[0].name).replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, ""))
-                        state.previewUrl.push(result.tracks[0].preview_url)
+                    if (result.tracks[0] !== null) {
+                        state.similarArtistNames.push(result.tracks[0].artists[0].name);
+                        state.similarArtistImages.push(result.tracks[0].album.images[0].url);
+                        state.topTrackNames.push((result.tracks[0].name).replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, ""));
+                        state.previewUrl.push(result.tracks[0].preview_url);
                         if (state.similarArtistNames.length == state.numberOfResults) {
                             for (var i = 0; i < state.numberOfResults; i++) {
                                 getYoutubeSearch(state.topTrackNames[i], state.similarArtistNames[i], '-cover');
                             }
                         }
-                    }
-                    else if (result.tracks[0] == null){
+                    } else if (result.tracks[0] === null) {
                         noResults('.no-results');
                     }
                 });
-        })
+        });
     };
 
     //YOUTUBE API
+    //Takes the name of the track as well as the name of the artist and searches for videos. Stores each videos URL in the state.
     function getYoutubeSearch(q, name, musicVideo) {
-        // console.log ("GETTING YT SEARCH")
         return $.get('https://www.googleapis.com/youtube/v3/search', {
             part: 'snippet',
             key: 'AIzaSyAs_Lal_n3-LakD3xnUmFqKRzhgJiiMifI',
@@ -112,20 +105,21 @@ $(document).ready(function() {
             type: 'video',
         }).then(function(result) {
             if (result.items.length === 0) {
-                state.topTracksYouTube.push('')
+                state.topTracksYouTube.push('');
             } else {
-                state.topTracksYouTube.push(result.items[0].id.videoId)
+                state.topTracksYouTube.push(result.items[0].id.videoId);
             }
-            displaySimilarArtistNames(('.col-4'), state.similarArtistNames)
-        })
+            displaySimilarArtistNames(('.col-4'), state.similarArtistNames);
+        });
     }
-    //LYRICSWIKIA API
 
     //FUNCTIONS THAT RENDER STATE
+    //Displays the artists album cover, name, and top track and includes a link to the spotify sample of the track.
+    //If YouTube provides a music video, it will link to youtube instead of spotify.
     var displaySimilarArtistNames = function(element, names) {
         $(element).html('');
         for (var i = 0, n = i + state.numberOfResults; i < state.numberOfResults; i++) {
-            $(element).eq(n).remove()
+            $(element).eq(n).remove();
             $(element).eq(i).html(`
                 <div class="listen" style="background-image:url(${state.similarArtistImages[i]})">
                     <p class="artist-name"><b>${state.similarArtistNames[i]}</b></br>
@@ -134,8 +128,8 @@ $(document).ready(function() {
                     <button class="icons buttonbg" onclick="document.getElementById('sound1').play();">
                     <img class="icons" src="images/play-button.png" alt="Preview Track with Spotify">Spotify Sample
                     </button>
-                    </div>`)
-            if (state.topTracksYouTube[i] != '') {
+                    </div>`);
+            if (state.topTracksYouTube[i] !== '') {
                 $(element).eq(i).html(`
                     <div class="listen" style="background-image:url(${state.similarArtistImages[i]})">
                         <p class="artist-name"><b>${state.similarArtistNames[i]}</b></br>
@@ -143,17 +137,19 @@ $(document).ready(function() {
                         <a href="https://www.youtube.com/watch?v=${state.topTracksYouTube[i]}" target="_blank">
                             <img class="icons" src="images/play-button.png" alt="Watch top track on YouTube">
                         </a>
-                    </div>`)
+                    </div>`);
             }
 
         }
-        setTimeout(resubmitter, 1000)
+        setTimeout(resubmitter, 1000);
     };
+    //Function that displays a no results found if the spotify API doesn't respond with anything.
     var noResults = function(element) {
         $('.js-gears').fadeOut(100).addClass('hidden');
-        $('.no-results').html(`<h2>No results found. You either spelled it incorrectly or the artist is too obscure to get reliable results.</h2>`)
-    }
+        $('.no-results').html(`<h2>No results found. You either spelled it incorrectly or the artist is too obscure to get reliable results.</h2>`);
+    };
 
+    //A little hack to get the Youtube API responses to be in the proper order.
     var resubmitter = function() {
         if (state.resubmit === 0) {
             $('#js-search-form').submit();
@@ -163,6 +159,5 @@ $(document).ready(function() {
             $('.js-gears').fadeOut(100).addClass('hidden');
             $('.col-4').removeClass('hidden').fadeIn(500);
         }
-    }
-
-})
+    };
+});
